634895be5b128cd92c8eaa5e02d33f1cb6b60656
starrybamboo <735845305@qq.com>
2025-03-13 08:12:31 +0800

    feat：解决了electron的打包，现在可以生成exe文件了，终于！

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8888,
+    cors: true, // 允许跨域
+    hmr: true, // 开启热更新
+  },
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
